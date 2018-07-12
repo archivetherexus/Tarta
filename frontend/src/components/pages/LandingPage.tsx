3eba48ac0f4be98326d1e35a2980e54934848ea6
Lain <koneko@toujou.drekkar.com>
2018-07-12 02:53:01 +0200

    Trying to improve the landing page.

--- frontend/src/components/pages/LandingPage.tsx
+++ frontend/src/components/pages/LandingPage.tsx
@@ -3,20 +3,28 @@ import { connect } from 'inferno-redux';
 import { Link } from 'inferno-router';
 
 class LandingPage extends Component<any, any> {
+    
+    handleClickLoginButton = () => this.context.router.history.push('/login');
+    handleClickFindOutMore = () => this.context.router.history.push('/feed'); // TODO: Point to the correct path.
+    
     render() {
+        const { i18n } = this.props;
         return (
             <div className="landing-page panel">
-                <Link to="/login">Please login first</Link>
-                <br />
-                <br />
-                {'Welcome to this landing page!'}
-                <br />
-                <Link to="/feed">{'Feed'}</Link>
-                <br />
+                <div className="main-title">
+                    {'Tarta is a new way to interact with your school!'}
+                    <br /><br />
+                    <button onClick={this.handleClickLoginButton} className="sugar-button login-button equal-button-width">{i18n('Login now')}</button>
+                    <br />
+                    {'or'}
+                    <br />
+                    <button onClick={this.handleClickFindOutMore} className="sugar-button find-out-more-button equal-button-width">{i18n('Find out more')}</button>
+                </div>
             </div>
         );
     }
 }
 
 export default connect((state:any) => ({
+    i18n: state.i18n,
 }))(LandingPage);
